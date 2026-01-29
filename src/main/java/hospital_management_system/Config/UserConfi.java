4396da8d5949b3e7d136cafbf21df30ea8578073
yaseenpatelsd <yaseenpatelsd@gmail.com>
2026-01-29 20:55:41 +0530

    Update UserConfi.java

--- src/main/java/hospital_management_system/Config/UserConfi.java
+++ src/main/java/hospital_management_system/Config/UserConfi.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableMethodSecurity
@@ -84,4 +85,5 @@ public class UserConfi {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
+
 
